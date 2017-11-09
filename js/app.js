d7813050afd28f3e8e1c1ade55408c1484364b33
mfullford <marissafullford@gmail.com>
2017-11-08 18:49:45 -0700

    Cursor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+
+
+$("body").css('cursor', 'url(./image/arrow.png) 2 2, auto');
+
+
 (function($) {
   "use strict"; // Start of use strict
+
+
 
   // Closes the sidebar menu
   $("#menu-close").click(function(e) {
@@ -59,23 +66,4 @@
 
 })(jQuery); // End of use strict
 
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+
